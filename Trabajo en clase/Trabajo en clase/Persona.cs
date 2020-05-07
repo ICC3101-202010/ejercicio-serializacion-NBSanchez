6f9ee05fc70621b65d4c0fe32ef995dfcb0be55e
NBSanchez <Nbsanchez@miuandes.cl>
2020-05-07 10:19:30 -0400

    SegundoIntento

--- Trabajo en clase/Trabajo en clase/Persona.cs
+++ Trabajo en clase/Trabajo en clase/Persona.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Trabajo_en_clase
 {
+    [Serializable]
     class Persona
     {
+        private string Name;
+        private string LastName;
+        private int Age;
+
+
+        public Persona(string Name, string LastName, int Age)
+        {
+            this.Name = Name;
+            this.LastName = LastName;
+            this.Age = Age;
+        }
+
     }
 }
--- Trabajo en clase/Trabajo en clase/Program.cs
+++ Trabajo en clase/Trabajo en clase/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,59 @@ namespace Trabajo_en_clase
     {
         static void Main(string[] args)
         {
+            List<Persona> ListaPersonas = new List<Persona>();
+            
+
+            void Almacenar (List<Persona> almacenar)
+            {
+                IFormatter formatter = new BinaryFormatter();
+                Stream stream = new FileStream("TBCLASE.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                formatter.Serialize(stream, almacenar);
+                stream.Close();
+            }
+            void Cargar()
+            {
+
+            }
+           
+            while (true)
+            {
+
+                Console.WriteLine("ESCOJA EL NUMERO DE ALGUNA DE LAS SIGUIENTES OPCIONES QUE TENEMOS PARA USTED: ");
+                Console.WriteLine("");
+                Console.WriteLine("1. CARGAR: ");
+                Console.WriteLine("2. SERIALIZAR: ");
+                Console.WriteLine("3. CREAR PERSONA: ");
+                Console.WriteLine("4. MOSTRAR PERSONAS: ");
+                Console.WriteLine("E. SALIR DEL PROGRAMA: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "3")
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("DAME LOS DATOS DE LA PERSONA EN EL SIGUIENTE ORDEN: NOMBRE,APELLIDO,EDAD");
+                    string Nombre = Console.ReadLine();
+                    string Apellido = Console.ReadLine();
+                    string Edad = Console.ReadLine();
+                    Persona persona = new Persona(Nombre, Apellido, Int32.Parse(Edad));
+                    ListaPersonas.Add(persona);
+
+
+
+                }
+                if (choice == "4")
+                {
+                    for(int x = 0; x < ListaPersonas.Count(); x++)
+                    {
+                        Console.WriteLine(ListaPersonas[x]);
+                    }
+                }
+                else if (choice == "E" || choice == "e")
+                {
+                    Console.WriteLine("Chao");
+                    break;
+                }
+            }
         }
     }
 }
